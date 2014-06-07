b5045e935b73598c18c1348f2646c56d5ac4d739
pscuderi <pscuderi@gmail.com>
2014-06-06 23:24:09 -0400

    user login works

--- demo-war/src/main/java/com/pscuderi/appengineangulardemo/servlet/GetUserServlet.java
+++ demo-war/src/main/java/com/pscuderi/appengineangulardemo/servlet/GetUserServlet.java
@@ -1,19 +1,24 @@
 package com.pscuderi.appengineangulardemo.servlet;
 import java.io.IOException;
-import java.util.Date;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.pscuderi.appengineangulardemo.util.ServletUtils;
 
 @SuppressWarnings("serial")
 public class GetUserServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/json");
-
-
-		resp.getWriter().println(ServletUtils.toJson(new Date()));
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+        resp.setContentType("text/json");
+        if (user != null)
+        	resp.getWriter().println(ServletUtils.toJson(user));
+        else
+        	resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
 	}
 }
