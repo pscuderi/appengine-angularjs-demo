a09d6d20a8acab4884bc7a45c3fb1e2f43f3b604
pscuderi <pscuderi@gmail.com>
2014-06-07 19:29:21 -0400

    merged

--- demo-war/src/main/java/com/pscuderi/appengineangulardemo/servlet/LogoutServlet.java
+++ demo-war/src/main/java/com/pscuderi/appengineangulardemo/servlet/LogoutServlet.java
@@ -5,25 +5,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.users.User;
-import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
 @SuppressWarnings("serial")
 public class LogoutServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-	    UserService userService = UserServiceFactory.getUserService();
-	    
-	    resp.sendRedirect(userService.createLogoutURL("/"));
-	    
-//	    User user = userService.getCurrentUser();
-//	    if (user != null) {
-//	    	// return the user to home
-//	    	// in the future, might want to get the path and redirect accordingly
-//	    	resp.sendRedirect(userService.createLogoutURL("/"));
-//	    }
-//	    else {
-//	    	System.out.println("ERROR: user isn't logged in...why logout?");
-//	    }
+	    resp.sendRedirect(UserServiceFactory.getUserService().createLogoutURL("/"));
 	}
 }
